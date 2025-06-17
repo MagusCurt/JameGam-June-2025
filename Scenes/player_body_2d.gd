ba4793e5514e7b7c1c2f03681c95d456ed51b689
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-06-16 22:34:03 -0400

    ds

--- Scenes/player_body_2d.gd
+++ Scenes/player_body_2d.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const SPOUT_STRENGTH = -100.0
+const SPOUT_STRENGTH = -70.0
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
 
 
 func _physics_process(delta: float) -> void:
@@ -16,6 +17,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_pressed("Spout") :
 		velocity.y = SPOUT_STRENGTH
+		gpu_particles_2d.set_emitting(true)
+	if Input.is_action_just_released("Spout"):
+			gpu_particles_2d.set_emitting(false)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
